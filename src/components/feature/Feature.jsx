5c771d054615791537144ab96b4e3e94a7867b50
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-06 13:26:08 +0200

    Added wgpt3 container, feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Feature.scss'
 
-const Feature = () => {
+const Feature = ({ title, text}) => {
   return (
-    <div>Feature</div>
-  )
-}
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features__feature_title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      
+      <div className="gpt3__features__feature_text">
+        <p>{text}</p>
+      </div>
+    </div>
+)};
 
 export default Feature
