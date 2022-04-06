92615368eaf21d3251d5278ae6732de6d8e79e2a
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-06 23:43:42 +0200

    Added posibility container

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Possibility.scss'
+import posibbility from '../../assets/possibility.png'
+
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt3__posibbility section__padding">
+      <div className="gpt3__posibbility-image">
+        <img src={posibbility} alt="posibbility" />
+      </div>
+      <div data-aos="fade" data-aos-delay="100" className="gpt3__posibbility-container">
+        <p className="gpt3__posibbility-container__title">Request Early Access to Get Started</p>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      </div>
+    </div>
   )
 }
 
