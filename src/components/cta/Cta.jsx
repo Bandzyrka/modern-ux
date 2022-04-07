24594cd925982df0a1dfa0120a65ff688e393026
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-07 14:01:21 +0200

    Added cta component

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,15 @@ import './Cta.scss'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div data-aos="flip-up" data-aos-delay="600"className="gpt3__cta section__margin">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h4>Register today & start exploring the endless possiblities.</h4>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
